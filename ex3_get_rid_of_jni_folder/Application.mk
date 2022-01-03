889eab5955c7ebf9572a84ebf661aefb52a5be1a
m00head <m00head@yahoo.co.uk>
2022-01-03 20:22:01 +0000

    Update build files to work with newer Android NDK releases

--- ex3_get_rid_of_jni_folder/Application.mk
+++ ex3_get_rid_of_jni_folder/Application.mk
@@ -1,6 +1,6 @@
 APP_OPTIM := debug
 APP_ABI := armeabi-v7a
-APP_STL := stlport_static 
+APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions
 APP_PLATFORM := android-19
 APP_BUILD_SCRIPT := Android.mk
